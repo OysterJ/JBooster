dca034771c63769e5b82c8793f8d287580969640
JailBot134 <jailbot134@gmail.com>
2021-01-21 11:33:35 +0200

    Engines sound added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,14 +26,21 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-        } 
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("Rotate to the left");
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            print("Rotate to the right");
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward);
+            } else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward);
+            }
     }
 }
